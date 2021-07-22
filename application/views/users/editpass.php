2b0cf95d7a0d47eb1ef7232e062f685546ae5ae6
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-07-22 17:11:48 -0500

    Cambiar Contraseña como Area de Clientes

--- application/views/users/editpass.php
+++ application/views/users/editpass.php
@@ -1,49 +1,43 @@
 <?php $this->load->view('template/header.php'); ?>
-</head>
-<body>
-	<?php $this->load->view('template/menu.php'); ?>
-	
-    <div class="container">
-        <div class="row justify-content-center mt-3 pt-2"> <!-- mt-5 pt5 -->
-            <div class="col-md-12 disabled-padding"> <!-- col-md-7 -->            
+<body id="page-top">    
+    <?php $this->load->view('template/sidebar.php'); ?>
+    <?php $this->load->view('template/menu.php'); ?>
 
-                <div id="formularioRegistros">
-                        <!-- <h3 class="text-primary"><?php echo $title; ?></h3> -->
+    <!-- Container -->
+    <div class="container-fluid">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4"> <!-- mb-4 -->
+            <h1 class="h3 mb-0 text-gray-800">Cambiar Contrase&ntilde;a:</h1>
+        </div>
 
-                        <div class="card border-primary">
-                            <div class="card-header">Usuario: <?php echo $user[0]->name . " (" . $user[0]->email . ")" ?></div>
-                            <div class="card-body text-primary">
+        <div class="row">
+            <div class="col-lg-12">
+                
+                <form method="post" action="<?php echo base_url() ?>index.php/users/updatepass"> <!-- novalidate -->
+                    <div class="col-lg-12 form-group">
+                        <label>Contrase&ntilde;a:</label>
+                        <input type="password" name="password" value="" id="password" class="form-control" required> <!-- required -->                        
+                    </div>
+                    <div class="col-lg-12 form-group">
+                        <label>Confirmar contraseña:</label>
+                        <input type="password" name="password_confirmation" value="" id="password" class="form-control" required> <!-- required -->                        
+                    </div>
+                    <!-- <div class="col-lg-12 form-group"> -->
+                        <input type="submit" name="submit" value="Cambiar contrase&ntilde;a" class="btn btn-primary" style="width:49%;"></input>
+                        <a class="btn btn-primary" href="<?php echo base_url() ?>index.php/users/view" style="width:49%;">Atr&aacute;s</a>
+                    <!-- </div> -->
 
-                                <form method="post" action="<?php echo base_url() ?>index.php/users/updatepass"> <!-- novalidate -->
-                                    <div class="row form-group">
-                                        <label for="descripcion" class="col-form-label col-md-2">Contraseña:</label>
-                                        <div class="col-md-5">
-                                            <input type="password" name="password" value="" id="password" class="form-control" required> <!-- required -->
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <label for="descripcion" class="col-form-label col-md-2">Confirmar contraseña:</label>
-                                        <div class="col-md-5">
-                                            <input type="password" name="password_confirmation" value="" id="password" class="form-control" required> <!-- required -->
-                                        </div>
-                                    </div>
-                                    <h4>
-                                        <input type="submit" name="submit" value="Cambiar contraseña" class="btn btn-primary"></input>
-                                        <a class="btn btn-primary" href="<?php echo base_url() ?>index.php/users/view">Atras</a>
-                                    </h4>
+                    <input type="hidden" name="sec_user_id" value="<?php echo $user[0]->sec_user_id; ?>" class="form-control">
+                </form>
+                <div class="row">&nbsp;</div>
 
-                                    <input type="hidden" name="sec_user_id" value="<?php echo $user[0]->sec_user_id; ?>" class="form-control">
-                                </form>
+            </div>
+        </div>
+                  
+    </div>
+    <!-- Cerrar Container --> 
 
-                            </div>
-                        </div>
-                </div>                                                                                
-
-            <?php $this->load->view('template/footer.php'); ?>
-
-            </div><!-- .col -->
-        </div><!-- .row -->
-    </div><!-- .container -->
-
-<?php $this->load->view('template/scripts.php'); ?>
-<script src="<?php echo base_url() ?>assets/js/users.js"></script>
+    <?php $this->load->view('template/footer.php'); ?>
+    <?php $this->load->view('template/scripts.php'); ?>
+    <script src="<?php echo base_url() ?>assets/js/rucs.js"></script>
+</body>
+</html>
