a622e15b0d330070fab3ddbb96854aea14feadb5
Jorge Ernesto <jorge.cywdt@gmail.com>
2021-07-22 17:11:48 -0500

    Crear RUCs como Area de Clientes

--- application/views/rucs/create.php
+++ application/views/rucs/create.php
@@ -1,52 +1,47 @@
 <?php $this->load->view('template/header.php'); ?>
-</head>
-<body>
-	<?php $this->load->view('template/menu.php'); ?>
-	
-    <div class="container">
-        <div class="row justify-content-center mt-3 pt-2"> <!-- mt-5 pt5 -->
-            <div class="col-md-12 disabled-padding"> <!-- col-md-7 -->            
+<body id="page-top">    
+    <?php $this->load->view('template/sidebar.php'); ?>
+    <?php $this->load->view('template/menu.php'); ?>
 
-                <div id="formularioRegistros">
-                        <!-- <h3 class="text-primary"><?php echo $title; ?></h3> -->
+    <!-- Container -->
+    <div class="container-fluid">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4"> <!-- mb-4 -->
+            <h1 class="h3 mb-0 text-gray-800">Crear Usuarios</h1>
+        </div>
 
-                        <div class="card border-primary">
-                            <div class="card-header">RUCs</div>
-                            <div class="card-body text-primary">
+        <div class="row">
+            <div class="col-lg-12">
+                
+                <form role="form" method="post" action="<?php echo base_url() ?>index.php/rucs/store" style="width: 100%;">
+                    <div class="col-lg-12 form-group">
+                        <label>RUC:</label>
+                        <input type="number" name="ruc" value="" id="ruc" class="form-control" 
+                                maxlength="11" 
+                                pattern="[0-9]"
+                                oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+                                required> <!-- required -->
+                    </div>
+                    <div class="col-lg-12 form-group">
+                        <label>Razon Social:</label>
+                        <input type="text" name="razon_social" value="" id="razon_social" class="form-control" required> <!-- required -->                        
+                    </div>                                                                                                              
+                    <!-- <div class="col-lg-12 form-group"> -->
+                        <input type="submit" name="submit" value="Crear RUC" class="btn btn-primary" style="width:49%;"></input>&nbsp;
+                        <a class="btn btn-primary" href="<?php echo base_url() ?>index.php/users/edit/<?php echo $user[0]->sec_user_id; ?>" style="width:49%;">Atr&aacute;s</a>
+                    <!-- </div> -->
 
-                                <form method="post" action="<?php echo base_url() ?>index.php/rucs/store"> <!-- novalidate -->
-                                    <div class="row form-group">
-                                        <label for="nombre" class="col-form-label col-md-2">RUC:</label> <!-- col-md-4 -->
-                                        <div class="col-md-5"> <!-- col-md-8 -->
-                                            <input type="number" name="ruc" value="" id="ruc" class="form-control" 
-                                            maxlength="11" 
-                                            oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
-                                            required> <!-- required -->                                            
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <label for="descripcion" class="col-form-label col-md-2">Razon Social:</label>
-                                        <div class="col-md-5">
-                                            <input type="text" name="razon_social" value="" id="razon_social" class="form-control" required> <!-- required -->
-                                        </div>
-                                    </div>                                                                                                              
-                                    <h4>
-                                        <input type="submit" name="submit" value="Crear RUC" class="btn btn-primary"></input>
-                                        <a class="btn btn-primary" href="<?php echo base_url() ?>index.php/users/edit/<?php echo $user[0]->sec_user_id; ?>">Atras</a>
-                                    </h4>
+                    <input type="hidden" name="sec_user_id" value="<?php echo $user[0]->sec_user_id; ?>" id="id" class="form-control">
+                </form>
+                <div class="row">&nbsp;</div>
 
-                                    <input type="hidden" name="sec_user_id" value="<?php echo $user[0]->sec_user_id; ?>" id="id" class="form-control">
-                                </form>
+            </div>
+        </div>
+                  
+    </div>
+    <!-- Cerrar Container --> 
 
-                            </div>
-                        </div>
-                </div>                                                                                
-
-            <?php $this->load->view('template/footer.php'); ?>
-
-            </div><!-- .col -->
-        </div><!-- .row -->
-    </div><!-- .container -->
-
-<?php $this->load->view('template/scripts.php'); ?>
-<script src="<?php echo base_url() ?>assets/js/users.js"></script>
+    <?php $this->load->view('template/footer.php'); ?>
+    <?php $this->load->view('template/scripts.php'); ?>
+    <script src="<?php echo base_url() ?>assets/js/users.js"></script>
+</body>
+</html>
